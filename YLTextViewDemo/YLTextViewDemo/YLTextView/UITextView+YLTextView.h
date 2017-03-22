fe2166439d1184cb48b89428d1456a5ab5b8cc3a
Riandew <zhangyuluios@163.com>
2017-03-22 12:54:59 +0800

    Update UITextView+YLTextView.h

--- YLTextViewDemo/YLTextViewDemo/YLTextView/UITextView+YLTextView.h
+++ YLTextViewDemo/YLTextViewDemo/YLTextView/UITextView+YLTextView.h
@@ -19,6 +19,7 @@
 
 /*  ！！！！！！！ 特殊说明。如果你想对textView.text直接赋值  ！！！！！！！！
  *       请务必在对textView.placehoulder和textView.limitLength之前进行
+         也就是说，一旦你对text赋值了，你需要立马重新对placeholder赋值。
  *       格式：      textView.text = @"请务必写在placeholder和limitLength之前";
  *                  textView.placeholder = @"喜欢请Star";
  *                  textView.limitLength = @20;//如果赋值长度大于此长度会被自动截断！
