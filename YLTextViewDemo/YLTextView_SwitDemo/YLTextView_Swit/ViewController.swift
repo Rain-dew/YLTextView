f071c992879203124b00a42eeabbd8e0575dea28
张雨露 <88396264@cnsuning.com>
2020-08-05 11:12:15 +0800

    fix

--- YLTextViewDemo/YLTextView_SwitDemo/YLTextView_Swit/ViewController.swift
+++ YLTextViewDemo/YLTextView_SwitDemo/YLTextView_Swit/ViewController.swift
@@ -13,11 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
-        let textview = UITextView(frame: CGRect(x: 100, y: 100, width: 200, height: 150))
+        let textview = UITextView(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
 //        textview.text = "如果你想对textView.text直接赋值。请在设置属性之前进行，否则影响计算"
         textview.placeholder = "喜欢请Star"
-//        textview.limitLength = 20
-        textview.limitLines = 2;
+        textview.limitLength = 200
+//        textview.limitLines = 2
+        textview.autoHeight = true
         textview.center = self.view.center
         view.addSubview(textview)
     }
