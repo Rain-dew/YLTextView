ae95e7f6c5935823dc7e623a64ae1bc70618a175
Raindew <zhangyuluios@163.com>
2022-08-23 11:28:47 +0800

    Update UITextView+YLTextView.h
    删除备注

--- YLTextViewDemo/YLTextViewDemo/YLTextView/UITextView+YLTextView.h
+++ YLTextViewDemo/YLTextViewDemo/YLTextView/UITextView+YLTextView.h
@@ -37,12 +37,4 @@ typedef void(^textViewInfo)(NSString *text,CGSize textViewSize);
 
 @property(nonatomic,   copy) textViewInfo infoBlock;//获取当前字数、当前高度的block
 
-/* ******特殊说明。如果你想对textView.text直接赋值*******
- *       请务必在对placehoulder和limitLength之前进行
- *       一旦你对text赋值了，你需要立马重新对placeholder赋值。
- *       格式：
- *             textView.text = @"请务必写在placeholder和limitLength之前";
- *             textView.placeholder = @"喜欢请Star";
- *             textView.limitLength = @20;//如果赋值长度大于此长度会被自动截断！
- */
 @end
